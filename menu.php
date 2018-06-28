a37a7bbb22bef72601574c3e5bd7446e66694440
Nikohla44 <lemellec@live.fr>
2018-06-28 09:24:06 +0200

    passage en php et creation d'un fichier menu à utiliser dans toutes les pages

---
+++ menu.php
@@ -0,0 +1,16 @@
+<div class="LeftSideMenu element">
+    <nav role="navigation">
+    <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+            <a href="index.php"><li>Accueil</li></a>
+            <a href="about.php"><li>À propos</li></a>
+            <a href="#"><li>Portfolio</li></a>
+            <a href="#"><li>Contact</li></a>
+        </ul>
+    </div>
+    </nav>
+</div>
