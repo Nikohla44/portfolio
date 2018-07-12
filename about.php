f203aa2fbac8b75cac95ba75e31bb13112f9a26f
Nikohla44 <lemellec@live.fr>
2018-07-12 16:59:08 +0200

    ajout de la page about.php et mise en place css

--- about.php
+++ about.php
@@ -6,11 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>À propos</title>
 
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/about.css">
 
 </head>
 
@@ -19,6 +20,39 @@
     <?php
         include 'menu.php';
     ?>
+
+    <div class="aboutContainer">
+
+        <div class="title">
+            <h1>À PROPOS</h1>
+        </div>
+
+        <hr class="divider">
+
+        <div class="subTitle">
+            <h2>QUI SUIS-JE ?</h2>
+        </div>
+
+        <div class="myPresentation">
+            <p>Mon nom est Nicolas Le Mellec, j’ai 34 ans et suis captivé par le monde du numérique depuis mon plus jeune âge.</p>
+            <p>Fin 2017, j’ai commencé pendant mon temps libre  une autoformation sur les bases du développement web avec OpenClassrooms. Je me suis, évidemment, très vite passionné par le code de programmation et j’ai pris la décision de faire de cette passion un métier, j’ai donc passé les sélections pour le centre de formation Arinfo et intégré en février 2018 la formation développeur web , labellisée « Grande école du Numérique ».</p>
+            <p>En 6 mois de formation intensive, l’ascension de mes connaissances et de mes  compétences a été fulgurante.</p>
+            <p>Je suis passionné, force de proposition, autodidacte et je continue de me former sur de nouvelles technologies innovantes.</p>
+            <p>Aujourd’hui je souhaite mettre mes compétences au sein d’une équipe dynamique afin de continuer d’évoluer, d’apprendre et de m’améliorer.</p>
+        </div>
+
+        <div class="cv">
+            <a href="images/LeMellec.C.V..pdf" target=_blank><input type="submit" class="button2" value="Mon C.V."></a>
+        </div>
+
+        <hr class="divider">
+
+        <div class="subTitle">
+            <h2>MES COMPÉTENCES</h2>
+        </div>
+
+
+    </div>
     
 </body>
 
